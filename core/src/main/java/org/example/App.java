687d60dfd7174fe7b3ec12382b3d9f699a853092
SangeevanRumex <sangeevan@rumex.lk>
2021-06-15 12:43:47 +0530

    Create the following multi-module project using maven interactive mode

---
+++ core/src/main/java/org/example/App.java
@@ -0,0 +1,15 @@
+package org.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Core Module" );
+        System.out.println( "Hello Sangeevan!" );
+        System.out.println( "Wel Come..." );
+    }
+}
---
+++ web/src/main/webapp/index.jsp
@@ -0,0 +1,7 @@
+<html>
+<body>
+<h1>Web Module</h1>
+<h2>Hello Sangeevan!</h2>
+<h3>Wel Come...</h3>
+</body>
+</html>
