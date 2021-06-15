2de0b60ae56d1c76bec6a3365ad02383c0c2d39b
SangeevanRumex <sangeevan@rumex.lk>
2021-06-15 15:47:50 +0530

    Change core module content

--- core/src/main/java/org/example/App.java
+++ core/src/main/java/org/example/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Core Module" );
+        System.out.println( "This is Core Module" );
         System.out.println( "Hello Sangeevan!" );
         System.out.println( "Wel Come..." );
     }
